a47966a1d0b991f57f8d3cf6b115f75042488481
Krupa Patel <patelkrupa1806@gmail.com>
2023-07-24 18:13:17 +0530

    1.1

---
+++ src/main/kotlin/Practical1.1.kt
@@ -0,0 +1,21 @@
+fun main()
+{
+    var a : Int = 22
+    var b = 1.5
+    var c : Char = 'T'
+    var d : String = "HMS"
+    var e : Boolean = false
+    var f : Double = 96.36
+    val g : Long = 338927847
+    val h : Short = -2
+    val i : Byte = 127
+    println("Integer Value: $a")
+    println("Float Value: $b")
+    println("Char Value: $c")
+    println("String Value: $d")
+    println("Boolean Value: $e")
+    println("Double Value: $f")
+    println("Long Value: $g")
+    println("Short Value: $h")
+    println("Byte Value: $i")
+}
